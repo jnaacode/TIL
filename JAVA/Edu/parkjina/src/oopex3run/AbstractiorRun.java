e18c5e7d50f8ef99f4ace2b0e25b638996bf8b20
recordbuffer <jnaacode@gmail.com>
2023-04-30 20:10:26 +0900

    4/30일 객체지향

---
+++ JAVA/Edu/parkjina/src/oopex3run/AbstractiorRun.java
@@ -0,0 +1,14 @@
+package oopex3run;
+
+import oopex1.Abstractior;
+
+public class AbstractiorRun {
+	
+	public static void main(String[] args) {
+		
+		Abstractior as = new Abstractior();
+		
+//		as.ex1();
+	}
+
+}
